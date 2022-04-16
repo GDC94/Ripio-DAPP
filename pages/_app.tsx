189b029547b7fee132008a996d92f1e4bfd43b06
gdc94 <germanderbescatoni@gmail.com>
2022-04-15 23:51:33 -0300

    refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,30 +3,29 @@ import { AppProps } from "next/app";
 import React from "react";
 import theme from "../theme";
 import { Fonts } from "../fonts/Fonts";
-import { Web3ReactProvider } from "@web3-react/core";
-import { getLibrary } from "../config/web3/Index";
+
 import { MoralisProvider } from "react-moralis";
 import { motion } from "framer-motion";
-import { Router } from "next/router";
-
-
+const server = "https://rc7zjxjwv14f.usemoralis.com:2053/server";
+const appId = "buVUlvsXshDcaSheimkZc78iaMdtaFb607KTvKwo";
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <MoralisProvider
-        appId={process.env.NEXT_PUBLIC_APP_ID}
-        serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
-      >
-      <motion.div  initial='pageInitial' animate='pageAnitame' variants={{
-        pageInitial: {
-          opacity: 0
-        },
-        pageAnitame: {
-          opacity: 1.3
-        }
-      }}>
-        <Fonts />
-        <Component {...pageProps} />
+      <MoralisProvider appId={appId} serverUrl={server}>
+        <motion.div
+          initial="pageInitial"
+          animate="pageAnitame"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnitame: {
+              opacity: 1.3,
+            },
+          }}
+        >
+          <Fonts />
+          <Component {...pageProps} />
         </motion.div>
       </MoralisProvider>
     </ChakraProvider>
